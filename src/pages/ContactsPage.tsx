42e84240d9db844e565a7af389c0372eb9cd8a5b
Kross <kross@sofia.ru>
2026-05-14 01:00:58 +0300

    Replace iframe with Yandex Maps JS constructor

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -169,16 +169,7 @@ export default function ContactsPage() {
 
           <div className="flex flex-col">
             <div className="bg-card rounded-2xl border border-border shadow-sm overflow-hidden flex-1 min-h-[400px] lg:min-h-0">
-              <iframe
-                src={mapUrl}
-                width="100%"
-                height="100%"
-                frameBorder="0"
-                title={getText('contacts-map-title')}
-                allowFullScreen
-                className="w-full h-full"
-                style={{ border: 0 }}
-              />
+              <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ab0f6bdba1bf40e61eb94b3385213c766d1d00889f4b9b4a464485f23875b0b5b&width=500&height=400&lang=ru_RU&scroll=true" />
             </div>
           </div>
         </div>
